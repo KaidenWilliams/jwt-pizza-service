68ee18a1216dcba91d6df8dea684c33ca41e936e
KaidenWilliams <kman16409@gmail.com>
2024-11-23 20:35:41 -0700

    changed CI, added metrics file

---
+++ src/metrics.js
@@ -0,0 +1,13 @@
+// TODO
+
+// a. HTTP requests by method/minute. Total Requests, Get Requests, Put Requests, Post Requests, Delete requests
+
+// b. Active Users
+
+// c. Authentication Attempts / minute (successful + failed)
+
+// d. CPU and Memory Usage (will use os.... )
+
+// e. Pizza revenue. i. Sold/Minute, ii. Creation Failures, iii. Revenue / Minute
+
+// d. Latency i. Service Endpoint. ii. Pizza Creation
