158aeb5919f236a3f4ec0afaf039545a16f02706
KaidenWilliams <kman16409@gmail.com>
2024-11-23 21:49:39 -0700

    changes

--- src/metrics.js
+++ src/metrics.js
@@ -11,3 +11,64 @@
 // e. Pizza revenue. i. Sold/Minute, ii. Creation Failures, iii. Revenue / Minute
 
 // d. Latency i. Service Endpoint. ii. Pizza Creation
+
+const config = require("./config.json");
+
+class Metrics {
+  constructor() {
+    this.requests = {
+      all: 0,
+      get: 0,
+      post: 0,
+      put: 0,
+      delete: 0,
+    };
+
+    this.startMetricsTimer();
+  }
+
+  requestTracker(req) {
+    const method = req.method.toLowerCase();
+
+    this.requests.all++;
+    if (this.requests[method !== undefined]) {
+      this.requests[method]++;
+    }
+  }
+
+  startMetricsTimer() {
+    const timer = setInterval(() => {
+      this.sendEveryRequest();
+    }, 10000);
+    timer.unref();
+  }
+
+  sendEveryRequest() {
+    for (const method in this.requests) {
+      this.sendMetricToGrafana("request", method, "total", this.requests[method]);
+    }
+  }
+
+  sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
+    const metric = `${metricPrefix},source=${config.source},method=${httpMethod} ${metricName}=${metricValue}`;
+
+    fetch(`${config.url}`, {
+      method: "post",
+      body: metric,
+      headers: { Authorization: `Bearer ${config.userId}:${config.apiKey}` },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error("Failed to push metrics data to Grafana");
+        } else {
+          console.log(`Pushed ${metric}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Error pushing metrics:", error);
+      });
+  }
+}
+
+const metrics = new Metrics();
+module.exports = metrics;
